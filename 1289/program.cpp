f1d651b7b02239e68460de1334bd70fc805c5d92
Márton Reiter <marci.reiter501@gmail.com>
2024-04-29 11:00:04 +0200

    [1289] Making changes

--- 1289/program.cpp
+++ 1289/program.cpp
@@ -4,40 +4,35 @@
 class Solution {
 public:
     int minFallingPathSum(std::vector<std::vector<int>>& grid) {
-        std::vector<int> first_row = grid[0];
-        int smallest = first_row[0];
-        size_t smallest_index = 0;
-
-        for (size_t i = 0; i < first_row.size(); ++i) {
-            if (first_row[i] < smallest) {
-                smallest = first_row[i];
-                smallest_index = i;
-            }
-        }
-
-        return smallest + subMinFallingPathSum(smallest_index, grid);
+        return subMinFallingPathSum(0, grid, true);
     }
 
-    int subMinFallingPathSum(size_t currently_taken_index, std::vector<std::vector<int>>& grid){
+    int subMinFallingPathSum(size_t last_smallest_index, std::vector<std::vector<int>>& grid, bool is_first_row){
+        if (grid.size() == 0) return 0;
+
         std::vector<int> first_row = grid[0];
         int smallest = first_row[0];
-        size_t new_index = 0;
+        size_t current_smallest_index = 0;
 
         for (size_t i = 0; i < first_row.size(); ++i) {
-            if (i != currently_taken_index) {
+            if (is_first_row) {
                 if (first_row[i] < smallest) {
                     smallest = first_row[i];
-                    new_index = i;
+                    current_smallest_index = i;
+                }
+            } else {
+                if (i != last_smallest_index) {
+                    if (first_row[i] < smallest) {
+                        smallest = first_row[i];
+                        current_smallest_index = i;
+                    }
+                    // DOES NOT WORK? WHAT IF THERE ARE MORE SAME SIZE SMALLEST ONES???!
                 }
             }
         }
 
-        if (grid.size() == 1) return smallest;
-
-        std::vector<std::vector<int>>& new_grid = grid;
-        new_grid.erase(new_grid.begin());
-
-        return smallest + subMinFallingPathSum(new_index, new_grid);
+        grid.erase(grid.begin());
+        return smallest + subMinFallingPathSum(current_smallest_index, grid, false);
     }
 };
 
