0bd596ce78a470a877b8dd37e534f5510134d34e
Márton Reiter <marci.reiter501@gmail.com>
2024-09-12 13:05:29 +0200

    Random push

--- 1289/program.cpp
+++ 1289/program.cpp
@@ -8,10 +8,13 @@ public:
         std::vector<int> first_row = grid[0];
         grid.erase(grid.begin());
 
+        // Create a memoization table to store the computed results
+        std::vector<std::vector<int>> memo(grid.size(), std::vector<int>(grid[0].size(), INT_MAX));
+
         int smallest_sum = INT_MAX;
 
         for (size_t i = 0; i < first_row.size(); ++i) {
-            int sum = first_row[i] + subMinFallingPathSum(i, grid);
+            int sum = first_row[i] + subMinFallingPathSum(i, grid, memo);
             if (sum < smallest_sum) {
                 smallest_sum = sum;
             }
@@ -20,8 +23,13 @@ public:
         return smallest_sum;
     }
 
-    int subMinFallingPathSum(size_t last_smallest_index, std::vector<std::vector<int>> grid){
+    int subMinFallingPathSum(size_t last_smallest_index, std::vector<std::vector<int>> grid, std::vector<std::vector<int>>& memo) {
         if (grid.size() == 0) return 0;
+
+        if (memo[0][last_smallest_index] != INT_MAX) {
+            // Return memoized value if it's already computed
+            return memo[0][last_smallest_index];
+        }
 
         std::vector<int> first_row = grid[0];
         grid.erase(grid.begin());
@@ -30,28 +38,35 @@ public:
 
         for (size_t i = 0; i < first_row.size(); ++i) {
             if (i != last_smallest_index) {
-                int sum = first_row[i] + subMinFallingPathSum(i, grid);
+                int sum = first_row[i] + subMinFallingPathSum(i, grid, memo);
                 if (sum < smallest_sum) {
                     smallest_sum = sum;
                 }
             }
         }
 
+        // Cache the computed result
+        memo[0][last_smallest_index] = smallest_sum;
+
         return smallest_sum;
     }
 };
 
 int main(){
-    std::vector<std::vector<int>> grid{ std::vector<int>{ -2,-18,31,-10,-71,82,47,56,-14,42 }, 
-        std::vector<int>{ 82,4,30,14,38 }, std::vector<int>{ -95,3,65,-7,64,75,-51,97,-66,-28 }, 
-        std::vector<int>{ -49,-3,-77,8,-19 }, std::vector<int>{ 36,3,-62,38,15,51,-58,-90,-23,-63 },
-        std::vector<int>{ -49,-3,-77,8,-19 }, std::vector<int>{ 58,-26,-42,-66,21,99,-94,-95,-90,89 },
-        std::vector<int>{ -49,-3,-77,8,-19 }, std::vector<int>{ 83,-66,-42,-45,43,85,51,-86,65,-39 },
-        std::vector<int>{ -49,-3,-77,8,-19 }, std::vector<int>{ 56,9,9,95,-56,-77,-2,20,78,17 },
-        std::vector<int>{ -49,-3,-77,8,-19 }, std::vector<int>{ 78,-13,-55,55,-7,43,-98,-89,38,90 },
-        std::vector<int>{ -49,-3,-77,8,-19 }, std::vector<int>{ 32,44,-47,81,-1,-55,-5,16,-81,17 },
-        std::vector<int>{ -49,-3,-77,8,-19 }, std::vector<int>{ -87,82,2,86,-88,-58,-91,-79,44,-9 },
-        std::vector<int>{ -49,-3,-77,8,-19 }, std::vector<int>{ -96,-14,-52,-8,12,38,84,77,-51,52 }};
+    // std::vector<std::vector<int>> grid{ std::vector<int>{ -2,-18,31,-10,-71,82,47,56,-14,42 }, 
+    //     std::vector<int>{ 82,4,30,14,38 }, std::vector<int>{ -95,3,65,-7,64,75,-51,97,-66,-28 }, 
+    //     std::vector<int>{ -49,-3,-77,8,-19 }, std::vector<int>{ 36,3,-62,38,15,51,-58,-90,-23,-63 },
+    //     std::vector<int>{ -49,-3,-77,8,-19 }, std::vector<int>{ 58,-26,-42,-66,21,99,-94,-95,-90,89 },
+    //     std::vector<int>{ -49,-3,-77,8,-19 }, std::vector<int>{ 83,-66,-42,-45,43,85,51,-86,65,-39 },
+    //     std::vector<int>{ -49,-3,-77,8,-19 }, std::vector<int>{ 56,9,9,95,-56,-77,-2,20,78,17 },
+    //     std::vector<int>{ -49,-3,-77,8,-19 }, std::vector<int>{ 78,-13,-55,55,-7,43,-98,-89,38,90 },
+    //     std::vector<int>{ -49,-3,-77,8,-19 }, std::vector<int>{ 32,44,-47,81,-1,-55,-5,16,-81,17 },
+    //     std::vector<int>{ -49,-3,-77,8,-19 }, std::vector<int>{ -87,82,2,86,-88,-58,-91,-79,44,-9 },
+    //     std::vector<int>{ -49,-3,-77,8,-19 }, std::vector<int>{ -96,-14,-52,-8,12,38,84,77,-51,52 }};
+
+    std::vector<std::vector<int>> grid { std::vector<int> { 1,2,3 },
+        std::vector<int> { 4,5,6 },
+        std::vector<int> { 7,8,9 }};
 
     Solution s;
     std::cout << "Solution: " << s.minFallingPathSum(grid) << std::endl;
